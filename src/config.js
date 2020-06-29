351b59ef034afacc455975bb49ce49ee104feaf3
Chris Talley <christopherr.talley@gmail.com>
2020-06-29 01:25:29 -0500

    Last configuration for deployment

--- src/config.js
+++ src/config.js
@@ -1,1 +1,1 @@
-export const api = process.env.NODE_ENV === 'development' ? 'http://localhost:5000' : 'http://localhost:5000'
+export const api = process.env.NODE_ENV === 'development' ? 'https://master.d2orb18e1brind.amplifyapp.com' : 'https://master.d2orb18e1brind.amplifyapp.com'
